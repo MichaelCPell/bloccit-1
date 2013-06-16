dd37903f2a0ff4764a675ae8d5eae871a73b323c
Aaron Brongersma <aaron.brongersma@gmail.com>
2013-06-16 01:05:58 -0400

    Basic Rails App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bloccit::Application.config.secret_token = '70c5f30cad5420890c12fb5e5765f4aa449b92fd881c3b72db82ae0f84692a916bd113c628dbb7cc069fc57438fe609f9e2b1da1daf030d00de53c0b5435a902'
